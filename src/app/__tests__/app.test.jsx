57630ecca86d9bf8f53bb614404eb1d22888f2f1
Jatin <jatin.parate@mlveda.com>
2021-10-17 14:36:14 +0530

    added app.js snapshot test

--- src/app/__tests__/app.test.jsx
+++ src/app/__tests__/app.test.jsx
@@ -1,4 +1,5 @@
 import { render } from '@testing-library/react';
+import { blog } from 'constants/routes';
 import { MemoryRouter } from 'react-router-dom';
 import { App } from '../app';
 import classes from '../app.module.scss';
@@ -12,4 +13,20 @@ test('It renders successfully', () => {
   const eles = container.getElementsByClassName(classes.app);
 
   expect(eles).toHaveLength(1);
+
+  expect(container.outerHTML).toMatchSnapshot();
+});
+
+test('It renders blogs page successfully', () => {
+  const { container } = render(
+    <MemoryRouter initialEntries={[{ pathname: blog }]}>
+      <App />
+    </MemoryRouter>,
+  );
+
+  const eles = container.getElementsByClassName(classes.app);
+
+  expect(eles).toHaveLength(1);
+
+  expect(container.outerHTML).toMatchSnapshot();
 });
